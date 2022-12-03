96046ebff40cc46d25bbd8d5a26d50aeaac5249c
Giovana <giovanaworkfg@gmail>
2022-12-03 09:48:19 -0300

    created new file

--- src/testandoAnimais/Animal.java
+++ src/testandoAnimais/Animal.java
@@ -1,4 +1,40 @@
 package testandoAnimais;
 
-public class Animal {
+public abstract class Animal {
+
+    //Atributos
+    private String name;
+    private int age;
+    private float weight;
+
+    //Métodos
+    public abstract void emitirSom();
+    public abstract void alimentar();
+    public abstract void locomover();
+
+    //Getters e Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
 }
---
+++ src/testandoAnimais/Ave.java
@@ -0,0 +1,47 @@
+package testandoAnimais;
+
+public class Ave extends Animal {
+
+    //Atributo
+    private String corPena;
+
+    //Getter e Setter
+    public String getCorPena() {
+        return corPena;
+    }
+
+    public void setCorPena(String corPena) {
+        this.corPena = corPena;
+    }
+
+    //Método da classe ave
+    public void corPena(){
+        if(corPena.equals("Azul")){
+            System.out.println("É a cor do céu");
+        } else if (corPena.equals("Amarelo")) {
+            System.out.println("É a cor do sol");
+        }else{
+            System.out.println("É uma cor muito bonita");
+        }
+    }
+
+
+
+
+    //Métodos sobrepostos
+    @Override
+    public void emitirSom() {
+        System.out.println("Emite som de ave");
+
+    }
+    @Override
+    public void alimentar() {
+        System.out.println("Se alimenta de bichinhos");
+
+    }
+    @Override
+    public void locomover() {
+        System.out.println("Se locomove voando");
+
+    }
+}
